a70ee041afddbff0c91f8af471addc86b2dd6ac4
Suhasr76 <suhasravi76@gmail.com>
2022-07-23 18:06:01 +0530

    Added `NextJS` framework template

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="text-5xl font-bold grid h-screen place-items-center">
+      Volunteer Management System
+    </div>
+  );
+};
+
+export default HomePage;
