3cd34813cc1df76f11777cacc6593984cb436731
Suhasr76 <suhasravi76@gmail.com>
2022-07-24 13:47:40 +0530

    Update

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <div>
+      <div className="w-[100vw] h-[90px] bg-gray-300"></div>
+    </div>
+  );
+}
+
+export default Navbar;
