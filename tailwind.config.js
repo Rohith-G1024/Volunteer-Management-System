a50c3eaaa99e5b9824e64672e7db8cb4b668b73e
Suhasr76 <suhasravi76@gmail.com>
2022-07-26 12:52:29 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -18,23 +17,5 @@ module.exports = {
         tealgrad: "url('/tealgrad.jpeg')",
       },
     },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      teal: "#0369a1",
-      sky: "#38bdf8",
-      darkblue: "#1e3a8a",
-      purple: "#3f3cbb",
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      "bubble-gum": "#ff77e9",
-      bermuda: "#78dcca",
-      grey: "#6b7280",
-      black: "#0f172a",
-    },
   },
-  plugins: [require("@tailwindcss/forms")],
 };
